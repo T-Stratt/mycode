97a2b811b40b5ef6f41c4a1258c765a8d49bc107
Thomas Stratton <Thomas.Wayne.Stratton@outlook.com>
2020-07-28 19:43:48 -0400

    Weather Adisor Code

--- Weather_Advisor.py
+++ Weather_Advisor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Asks the user the weather in there area.
+# Asks the user the weather.
 weather = input("What is the weather in your area today?")
 
 # Asks the user the temperature and creates it as an integer.
@@ -9,8 +9,11 @@ temp = int(input("What is the temperature in your area today?"))
 # Message reads the user input of 'weather' + appended variables.
 message = "Today the weather is " + weather + ". We recommend"
 
-# If input value is set of variables or integers then set result.
-if weather.lower() == "sunny" and temp >= 70:
+# Allows input of either upper or lower case and appends new message based on the previous parameters in place.
+
+if weather.lower() == "sunny" and temp >= 100:
+    message = message + ' drink water and put on sunscreen.'
+elif weather.lower() == "sunny" and temp >= 70 and temp <= 99:
     message = message + ' going to the beach!'
 elif weather.lower() == "partly cloudy" and temp >= 60:
     message = message + ' go out for a nice drive.'
@@ -22,5 +25,8 @@ elif weather.lower() == "snowy" and temp <= 32:
     message = message + ' go out and make a snowman.'
 else:
     message = message + ' make the best of the the day.'
+
+# Will print complete message with all variables, integers, and appendages in place.
+
 print(message)
 
