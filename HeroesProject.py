6af64ddc80640e9e7901755fd4ef3338a4034cb9
Thomas Stratton <Thomas.Wayne.Stratton@outlook.com>
2020-07-28 12:47:50 -0400

    learning about dictionaries

---
+++ HeroesProject.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+heroes= {"Spiderman":{"Strength": "strong af", "Intelligence": "above average", "Speed": "nimble"}, "Hulk":{"Strength": "infinite", "Intelligence": "zero", "Speed": "hella"}, "Ironman":{"Strength": "stronk", "Intelligence": "genius", "Speed": "supersonic"}}
+
+
