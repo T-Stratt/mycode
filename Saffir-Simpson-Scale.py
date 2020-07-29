e611c6bcb10d0f6f176fd77d7c8a799bb821cad0
Thomas Stratton <Thomas.Wayne.Stratton@outlook.com>
2020-07-29 11:01:18 -0400

    Updated code

--- Saffir-Simpson-Scale.py
+++ Saffir-Simpson-Scale.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
 
 # Input what the wind speed is.
-windspeed = int(input("What is the wind speed?"))
+windspeed = int(input("What is the wind speed?")) 
  
 # Outputs the wind speed and category of the storm.
-message = print("The speed is" + wind speed + " mph.The storm is a Cat")
+message ="The speed is " + str(windspeed) + " mph. "
 
 # Outputs message for if not within any other parameters.
 warning = "High winds and possible flooding."
 
 if windspeed >= 157:
-    message = message + ' 5.Catastrophic damage will occur.'
-elif windspeed  <= 156 and >= 130:
-    message = message + ' 4.Catastrophic damage will occur.'
-elif windspeed <= 129 and >= 111:
-    message = message + ' 3.Devastating damage will occur.'
-elif windspeed <= 110 and >= 96:
-    message = message + ' 2.Extremely dangerous winds will cause extensive damage.'
-elif windspeed <= 95 and >= 74:
-    message = message + ' 1.Very dangerous winds will produce some damage.'
+    message = message + ' The storm is a Cat 5.Catastrophic damage will occur.'
+elif windspeed >= 130:
+    message = message + ' The storm is a Cat 4.Catastrophic damage will occur.'
+elif windspeed >= 111:
+    message = message + ' The storm is a Cat 3.Devastating damage will occur.'
+elif windspeed >= 96:
+    message = message + ' The storm is a Cat 2.Extremely dangerous winds will cause extensive damage.'
+elif windspeed >= 74:
+    message = message + ' The storm is a Cat 1.Very dangerous winds will produce some damage.'
 else:
     warning = 'High winds and possible flooding.'
-print(message)
+
+print(message + warning)
 
