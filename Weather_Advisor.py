647c727dab105781b431774e02abbbf781c6fce0
Thomas Stratton <Thomas.Wayne.Stratton@outlook.com>
2020-07-28 21:07:38 -0400

    Weather Projects

--- Weather_Advisor.py
+++ Weather_Advisor.py
@@ -11,20 +11,24 @@ message = "Today the weather is " + weather + ". We recommend"
 
 # Allows input of either upper or lower case and appends new message based on the previous parameters in place.
 
-if weather.lower() == "sunny" and temp >= 100:
+if weather.lower() == "hot" and temp >= 100:
     message = message + ' drink water and put on sunscreen.'
-elif weather.lower() == "sunny" and temp >= 70 and temp <= 99:
+elif weather.lower() == "sunny" and temp >= 70:
     message = message + ' going to the beach!'
 elif weather.lower() == "partly cloudy" and temp >= 60:
     message = message + ' go out for a nice drive.'
+elif weather.lower() == 'stormy' and temp < 90 and temp > 40:
+    message = message + ' button down the hatch.'
 elif weather.lower() == "rainy" and temp >= 50:
     message = message + ' grab an umbrella.'
 elif weather.lower() == "cold" and temp >= 40:
-    message = message + ' cuddle in bed with your loved one.'
-elif weather.lower() == "snowy" and temp <= 32:
+    message = message + ' put on some extra layers.'
+elif weather.lower() == "snowy" and temp <= 32 and temp > 0:
     message = message + ' go out and make a snowman.'
+elif weather.lower() == "freezing" and temp < 0:
+    message = message + ' cuddle up with a loved one.'
 else:
-    message = message + ' make the best of the the day.'
+    message = message + ' make the best of the day.'
 
 # Will print complete message with all variables, integers, and appendages in place.
 
