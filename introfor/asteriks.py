d49857741c857b1915f21ae61da892a1a4e0558c
Thomas Stratton <Thomas.Wayne.Stratton@outlook.com>
2020-07-29 14:24:21 -0400

    studying for logic

---
+++ introfor/asteriks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# List of Pattern
+
+triangle = ["*", "**", "***", "****", "*****", "****", "***", "**", "*"]
+
+for x in triangle:
+    print(x)
+print("\n Pattern is complete.")
+
---
+++ introfor/farms.py
@@ -0,0 +1,4 @@
+farms = [{"name": "NE Farm", "agriculture": ["sheep", "cows", "pigs", "chickens", "llamas", "cats"]},
+         {"name": "W Farm", "agriculture": ["pigs", "chickens", "llamas"]},
+         {"name": "SE Farm", "agriculture": ["chickens", "carrots", "celery"]}]
+
